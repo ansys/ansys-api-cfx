7abe13dbb7816ff2ad0a1596f540111ddcbe8f1b
Ming Chen <ming.chen@ansys.com>
2024-06-20 16:27:03 -0400

    Address review comments.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
         long_description=long_description,
         long_description_content_type="text/markdown",
         license="MIT",
-        python_requires=">=3.7",
+        python_requires=">=3.9",
         install_requires=["grpcio~=1.30", "protobuf>=3.19,<5"],
         packages=setuptools.find_namespace_packages(".", include=("ansys.*",)),
         package_data={
