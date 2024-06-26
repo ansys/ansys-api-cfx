8bbc9c6f6072851a08f907d93a0f01da48ee7424
Ming Chen <ming.chen@ansys.com>
2024-06-26 15:55:59 -0400

    Update python version requirement.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         long_description_content_type="text/markdown",
         url=f"https://github.com/ansys-internal/{package_name}",
         license="MIT",
-        python_requires=">=3.9",
+        python_requires=">=3.9,<4.0",
         install_requires=["grpcio~=1.30", "protobuf>=3.19,<5"],
         package_dir={"": "src"},
         packages=setuptools.find_namespace_packages("src", include=("ansys.*",)),
